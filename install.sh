c375da7c78352c48d29643ec7d587330541c7fde
fardin1101 <sayyedfardin2003@gmail.com>
2025-04-17 17:39:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 # s3://my-java-app-artifacts-fardin/artifacts_shubham/LoginWebApp-1.war
 aws s3 cp s3://my-java-app-artifacts-fardin/artifacts_shubham/LoginWebApp-1.war /tmp
 sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
-sudo service tomcat restart
+sudo systemctl status tomcat
